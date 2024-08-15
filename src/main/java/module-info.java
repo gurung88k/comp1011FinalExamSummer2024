4d3699c0a5eb34a2eb74a7b07da6f124c723d422
gurung88k <gurungk4305@gmail.com>
2024-08-15 15:18:08 -0400

    q1 done

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.comp1011finalexamsummer2024 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.comp1011finalexamsummer2024 to javafx.fxml;
+    exports org.example.comp1011finalexamsummer2024;
+}
